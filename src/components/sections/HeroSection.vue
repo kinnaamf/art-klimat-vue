e1569b4be5580da62d71c71a57c251d8fd29b56c
kinnaamf <dimoje.1337@gmail.com>
2026-06-27 21:46:36 +0300

    feat: add hero section, add cta buttons

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -1,11 +1,26 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <section class="hero">
+    <div class="hero-content">
+      <div class="hero-text">
+        <h1 class="h1 !text-white">
+          Полный цикл вентиляции<br>с собственным производством
+        </h1>
+        <span class="text-big !text-white/70">
+          Лидер услуг от проекта до пусконаладки воздуховодов и фасонных изделий для инжиниринга<br>и строительства
+        </span>
+      </div>
+      <div class="hero-actions">
+        <BaseButton variant="primary" size="lg">Оставить заявку</BaseButton>
+        <BaseButton variant="secondary" size="lg">Наши услуги</BaseButton>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped lang="postcss">
+<style scoped>
 
 </style>
+
+<script setup lang="ts">
+import BaseButton from "@/components/ui/BaseButton.vue";
+</script>
---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="btn" :class="[ size, variant ]">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  variant?: 'primary' | 'secondary'
+  size?: 'md' | 'lg'
+}>()
+</script>
+
+<style scoped lang="postcss">
+
+</style>
