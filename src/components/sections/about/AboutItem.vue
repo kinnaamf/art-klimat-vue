e89805218a2617f7625297fd3f0f2445d5940414
kinnaamf <dimoje.1337@gmail.com>
2026-07-03 23:51:57 +0300

    fix: add text color transition on scroll

--- src/components/sections/about/AboutItem.vue
+++ src/components/sections/about/AboutItem.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import type { Stat } from "@/composables/useStats.ts";
+import { useIntersection } from "@/composables/useIntersection.ts";
+import { ref } from "vue";
 
 defineProps<{
   stat: Stat
 }>()
+
+const trigger = ref<HTMLElement | null>(null);
+
+const { isVisible: statsVisible } = useIntersection(trigger);
 </script>
 
 <template>
-  <div>
-    <h2 class="about__item-title">{{ stat.title }}</h2>
+  <div ref="trigger">
+    <h2
+        class="about__item-title"
+        :class="statsVisible ? 'text-orange' : 'text-orange/20'"
+    >{{ stat.title }}</h2>
     <span class="text-big">{{ stat.description }}</span>
   </div>
 </template>
