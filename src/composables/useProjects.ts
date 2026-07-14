4153718de5239385d6f1e9097bb91e316b90c9b3
kinnaamf <dimoje.1337@gmail.com>
2026-07-14 16:57:32 +0300

    fix: separate unit from area key

--- src/composables/useProjects.ts
+++ src/composables/useProjects.ts
@@ -11,51 +11,51 @@ export const useProjects = () => {
     {
       id: 1,
       title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 м',
-      unit: '<sup>2<sup>',
+      area: '16 000 ',
+      unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-1.png'
     },
     {
       id: 2,
       title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 м',
-      unit: '<sup>2<sup>',
-      image: '/images/completed-projects/image-1.png'
+      area: '16 000 ',
+      unit: 'м<sup>2<sup>',
+      image: '/images/completed-projects/image-2.png'
     },
     {
       id: 3,
       title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 м',
-      unit: '<sup>2<sup>',
-      image: '/images/completed-projects/image-1.png'
+      area: '16 000 ',
+      unit: 'м<sup>2<sup>',
+      image: '/images/completed-projects/image-3.png'
     },
     {
       id: 4,
       title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 м',
-      unit: '<sup>2<sup>',
-      image: '/images/completed-projects/image-1.png'
+      area: '16 000 ',
+      unit: 'м<sup>2<sup>',
+      image: '/images/completed-projects/image-4.png'
     },
     {
       id: 5,
       title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 м',
-      unit: '<sup>2<sup>',
-      image: '/images/completed-projects/image-1.png'
+      area: '16 000 ',
+      unit: 'м<sup>2<sup>',
+      image: '/images/completed-projects/image-5.png'
     },
     {
       id: 6,
       title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 м',
-      unit: '<sup>2<sup>',
-      image: '/images/completed-projects/image-1.png'
+      area: '16 000 ',
+      unit: 'м<sup>2<sup>',
+      image: '/images/completed-projects/image-6.png'
     },
     {
       id: 7,
       title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 м',
-      unit: '<sup>2<sup>',
-      image: '/images/completed-projects/image-1.png'
+      area: '16 000 ',
+      unit: 'м<sup>2<sup>',
+      image: '/images/completed-projects/image-7.png'
     },
   ]
 
