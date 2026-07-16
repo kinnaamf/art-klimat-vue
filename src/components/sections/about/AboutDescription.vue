7bab4b945cc50822ddef12456ee1c34394315bc3
kinnaamf <dimoje.1337@gmail.com>
2026-07-16 17:25:26 +0300

    feat: edit about description text

--- src/components/sections/about/AboutDescription.vue
+++ src/components/sections/about/AboutDescription.vue
@@ -2,7 +2,7 @@
 import AboutList from "@/components/sections/about/AboutList.vue";
 import { useStats } from "@/composables/useStats.ts";
 
-const { stats } = useStats();
+const {stats} = useStats();
 </script>
 
 <template>
@@ -14,15 +14,22 @@ const { stats } = useStats();
     </div>
     <div class="about__description-content">
       <div class="about__description-column">
-        <span class="text-big !text-darkgray">Также мы гарантируем качественное исполнение заказов в самые минимальные сроки</span>
-        <span class="text-big !text-darkgray">Если у вас имеются какие‑либо вопросы или пожелания, не стесняйтесь обращаться к нашим менеджерам</span>
+        <span class="text-big !text-darkgray">
+          ООО «ИК «Арт Климат» — инжиниринговая компания полного цикла, выполняющая проектирование, производство,
+          поставку, монтаж и сервисное сопровождение инженерных систем для коммерческих, промышленных,
+          общественных и жилых объектов.
+        </span>
+        <span class="text-big !text-darkgray">
+          Собственная производственная база и многолетний опыт позволяют выпускать стандартные и нестандартные решения,
+          гарантируя высокое качество, соблюдение сроков и надежность инженерных систем
+        </span>
       </div>
       <div>
         <span class="text-big !text-darkgray">Они предоставят вам необходимую помощь и проконсультируют по любым вопросам, связанным с вентиляционной техникой</span>
       </div>
     </div>
     <div>
-      <AboutList :stats="stats" />
+      <AboutList :stats="stats"/>
     </div>
   </div>
 </template>
