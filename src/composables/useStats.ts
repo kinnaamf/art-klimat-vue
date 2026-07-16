d1c51cb4d2a4579ef8653c2a5d6a6e616c351532
kinnaamf <dimoje.1337@gmail.com>
2026-07-16 17:19:43 +0300

    fix: change title type for stat

--- src/composables/useStats.ts
+++ src/composables/useStats.ts
@@ -1,24 +1,24 @@
 export interface Stat {
-  title: number
+  title: string
   description: string
 }
 
 export const useStats = () => {
   const stats: Stat[] = [
     {
-      title: 20,
+      title: '20',
       description: 'Лет на рынке'
     },
     {
-      title: 800,
+      title: '800+',
       description: 'Реализованных проектов'
     },
     {
-      title: 43,
+      title: '43',
       description: 'Сотрудников вштате'
     },
     {
-      title: 321,
+      title: '321',
       description: 'Отзывов клиентов'
     },
   ]
