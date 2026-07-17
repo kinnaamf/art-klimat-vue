aa7e281efb0632a9ff1f0a5a5b00d577f727c214
kinnaamf <dimoje.1337@gmail.com>
2026-07-17 21:27:34 +0300

    fix: edit projects title and area

--- src/composables/useProjects.ts
+++ src/composables/useProjects.ts
@@ -10,50 +10,50 @@ export const useProjects = () => {
   const projects: Project[] = [
     {
       id: 1,
-      title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 ',
+      title: 'МФК «Лахта центр»',
+      area: '70 000 ',
       unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-1.png'
     },
     {
       id: 2,
-      title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 ',
+      title: 'Новая Голландия',
+      area: '9 500 ',
       unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-2.png'
     },
     {
       id: 3,
-      title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 ',
+      title: 'Бизнес-центр «ЭНЕРГИЯ»',
+      area: '11 000 ',
       unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-3.png'
     },
     {
       id: 4,
-      title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 ',
+      title: 'Клубный дом «Три Грации»',
+      area: '20 000 ',
       unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-4.png'
     },
     {
       id: 5,
-      title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 ',
+      title: 'ДГМКЦ ВМТ им. К.А. Раухфуса',
+      area: '47 000 ',
       unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-5.png'
     },
     {
       id: 6,
-      title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 ',
+      title: 'Завод промышленного оборудования БПК',
+      area: '15 000 ',
       unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-6.png'
     },
     {
       id: 7,
-      title: 'Производство пищевых добавок «Русский Бейкелс»',
-      area: '16 000 ',
+      title: 'Ладожский вокзал',
+      area: '57 000 ',
       unit: 'м<sup>2<sup>',
       image: '/images/completed-projects/image-7.png'
     },
